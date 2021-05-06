10414ae0f21c676a32bc080df5efda43fd972eb0
Brycejax <bryce.martin726@hotmail.com>
2021-05-06 16:56:38 -0500

    created authors/courses components/services/modules

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,18 +1,27 @@
+import { CoursesService } from './courses.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
+import { AuthorsComponent } from './authors/authors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
