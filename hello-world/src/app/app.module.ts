e8cfe4bd08f14ed82661d0e5d073b6b8f63a58d0
Brycejax <bryce.martin726@hotmail.com>
2021-05-10 15:39:56 -0500

    I have refreshed myself on topics including the angular CLI, components, TS, databinding, stringinterpolation, property binding, and event binding.

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { WarningComponent } from './warningAlert/warning.component';
+import { ServerComponent } from './server/server.component';
 import { CoursesService } from './courses.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,17 +9,25 @@ import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { FormsModule} from '@angular/forms';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningComponent,
+    SuccessAlertComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     CoursesService
