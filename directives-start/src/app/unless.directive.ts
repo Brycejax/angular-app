6d20eb2adfe588d55a75235b212739166181dd1f
Brycejax <bryce.martin726@hotmail.com>
2021-05-17 21:33:08 -0500

    Took a deep dive on angular directives in the directives-start folder that was added

---
+++ directives-start/src/app/unless.directive.ts
@@ -0,0 +1,19 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appUnless]'
+})
+export class UnlessDirective {
+  @Input() set appUnless(condition: boolean) {
+    if (!condition) {
+      this.vcRef.createEmbeddedView(this.templateRef);
+    }
+    else {
+      this.vcRef.clear();
+    }
+  }
+
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
+
+}
